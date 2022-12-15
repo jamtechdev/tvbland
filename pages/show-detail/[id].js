0213d7f7ffe3989da1ac50836c26b65a18fea285
abhi2007it <abhi2007it@gmail.com>
2022-12-15 15:33:18 +0530

    Design Fixed

--- pages/show-detail/[id].js
+++ pages/show-detail/[id].js
@@ -36,10 +36,10 @@ export default function Single() {
             </div>
           </div>
           <div className="row mt-5">
-            <div className="col-xl-2 col-lg-3 col-md-6 col-12 ml-5 col-sm-12">
+            <div className="col-xl-3 col-lg-3 col-md-6 col-12 ml-5 col-sm-12 main_img_rme">
               <img src={show?.image.original} className="w-100 img-fluid" />
             </div>
-            <div className="col-xl-8 col-lg-8 col-md-6 col-12 shingle_sec_col mt-3 mb-3 ">
+            <div className="col-xl-9 col-lg-8 col-md-6 col-12 shingle_sec_col mt-3 mb-3 ">
               <div className="show-rating">
                 {show?.rating.average &&
                   <ReactStars size={20} value={show?.rating.average} edit={false} count={10} />
@@ -61,49 +61,46 @@ export default function Single() {
       {/* second-section-start */}
       <section>
         <div className="container mt-5">
-          <div className="row mt-3">
+          <div className="row mt-3 mb-3">
             <div className="col-xl-6 col-lg-6 col-md-6 col-12 mt-5 show_col_info">
               <h3>Show Info</h3>
               <ul className="list-group show_info ">
-                <li className="list-group-item d-flex  align-items-center">
-                  Language
+                <li className="list-group-item d-flex align-items-center">
+                <div className="type_main_heading">Language</div>
                   <span className="show_info_1">{show?.language}</span>
                 </li>
-                <li className="list-group-item  align-items-center">
-                  Status
-                  <span className="show_info_2">{show?.status}</span>
+                <li className="list-group-item d-flex align-items-center">
+                  <div className="type_main_heading">Status</div>
+                   <span className="show_info_2">{show?.status}</span>
                 </li>
-                <li className="list-group-item   align-items-center">
-                  Genres
-                  <span className="show_info_3">{show?.genres.join(',')}</span>
+                <li className="list-group-item d-flex align-items-center">
+                    <div className="type_main_heading">Genres</div>
+                    <span className="show_info_3">{show?.genres.join(',')}</span>
                 </li>
-                <li className="list-group-item   align-items-center">
-                  Schedule
-                  <span className="show_info_3">{show?.schedule.days}</span>
+                <li className="list-group-item d-flex align-items-center">
+                    <div className="type_main_heading">Schedule</div>
+                    <span className="show_info_3">{show?.schedule.days}</span>
                 </li>
-                <li className="list-group-item   align-items-center">
-                  Type
-                  <span className="show_info_3">{show?.type}</span>
+                <li className="list-group-item d-flex align-items-center">
+                    <div className="type_main_heading">Type</div>
+                    <span className="show_info_3">{show?.type}</span>
                 </li>
               </ul>
             </div>
-            <div className="col-xl-6 col-lg-6 col-md-6 col-12 mt-5">
-              <h3>Starting</h3>
+            <div className="col-xl-6 col-lg-6 col-md-6 col-12 mt-5 ">
+              <h3>Starring</h3>
               <ul className="list-group show_info1 ">
-                <li className="list-group-item  align-items-center ">
-                  <i className="fa-solid fa-user"></i>
-                  <span className="show_info_4">Starting</span>
-                  <span className="show_info_sp">BBC Theme</span>
+                <li className="list-group-item d-flex align-items-center ">
+                  <div className="user_profile_img"><i className="fa-solid fa-user"></i></div>
+                  <div className="d-flex main_lsit_name justify-content-between"><h6 className="mb-0">Starting</h6> <span>BBC Theme</span></div>
                 </li>
-                <li className="list-group-item  align-items-center ">
-                  <i className="fa-solid fa-user"></i>
-                  <span className="show_info_4">Starting</span>
-                  <span className="show_info_sp">BBC Theme</span>
+                <li className="list-group-item d-flex align-items-center ">
+                  <div className="user_profile_img"><i className="fa-solid fa-user"></i></div>
+                  <div className="d-flex main_lsit_name justify-content-between"><h6 className="mb-0">Starting</h6> <span>BBC Theme</span></div>
                 </li>
-                <li className="list-group-item  align-items-center ">
-                  <i className="fa-solid fa-user"></i>
-                  <span className="show_info_4">Starting</span>
-                  <span className="show_info_sp">BBC Theme</span>
+                <li className="list-group-item d-flex align-items-center ">
+                  <div className="user_profile_img"><i className="fa-solid fa-user"></i></div>
+                  <div className="d-flex main_lsit_name justify-content-between"><h6 className="mb-0">Starting</h6> <span>BBC Theme</span></div>
                 </li>
               </ul>
             </div>
